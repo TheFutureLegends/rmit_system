7154f74e1e0b3d9341863cac7a3c965879db6149
leonguyen0202 <leonguyen0202@gmail.com>
2020-09-23 21:12:52 +0700

    Modify layouts

--- routes/web.php
+++ routes/web.php
@@ -38,4 +38,6 @@ Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
 
 Route::post('/login', 'Auth\LoginController@login')->name('login');
 
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
+
 Route::get('/home', 'HomeController@index')->name('home');
